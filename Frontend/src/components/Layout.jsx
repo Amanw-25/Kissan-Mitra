74abafefb72646339209af3a98b482ecc4d737e5
Aman Wairagkar <124350777+Amanw-25@users.noreply.github.com>
2025-05-01 20:01:09 +0530

    Update Layout.jsx

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,3 +1,41 @@
+// import React, { Suspense } from "react";
+// import { Route, Routes, useLocation } from "react-router-dom";
+// import { privateRoutes, publicRoutes } from "../routes/Approutes";
+// import PrivateRoute from "../routes/Privateroute";
+// import Mainloader from "./shared/apploaders/Mainloader";
+// import Navigationbar from "./shared/navbar/Navigationbar";
+// import "./Layout.css";
+// import Footer from "./layouts/footer/Footer";
+// import { useSelector } from "react-redux";
+// import Header from "./layouts/header/Header";
+
+// const Layout = () => {
+//   const { isUserAuthenticated } = useSelector((state) => state.userauthstate);
+
+//   return (
+//     <Suspense fallback={<Mainloader />}>
+//       <header className="sticky top-0 z-[100]">
+//         {isUserAuthenticated ? <Navigationbar /> : <Header />}
+//       </header>
+//       <Routes>
+//         {publicRoutes.map(({ path, element }) => (
+//           <Route key={path} path={path} element={element} />
+//         ))}
+//         {privateRoutes.map(({ path, element }) => (
+//           <Route
+//             key={path}
+//             path={path}
+//             element={<PrivateRoute element={element} />}
+//           />
+//         ))}
+//       </Routes>
+//       <Footer />
+//     </Suspense>
+//   );
+// };
+
+// export default Layout;
+
 import React, { Suspense } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { privateRoutes, publicRoutes } from "../routes/Approutes";
@@ -15,7 +53,7 @@ const Layout = () => {
   return (
     <Suspense fallback={<Mainloader />}>
       <header className="sticky top-0 z-[100]">
-        {isUserAuthenticated ? <Navigationbar /> : <Header />}
+        <Navigationbar />
       </header>
       <Routes>
         {publicRoutes.map(({ path, element }) => (
@@ -35,3 +73,4 @@ const Layout = () => {
 };
 
 export default Layout;
+
