d06eed2eed304e45ca052ea47117841faabc6d52
Aman Wairagkar <124350777+Amanw-25@users.noreply.github.com>
2025-05-11 12:14:20 +0530

    Update Navigationbar.jsx

--- Frontend/src/components/shared/navbar/Navigationbar.jsx
+++ Frontend/src/components/shared/navbar/Navigationbar.jsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Navbar, Typography, IconButton } from "@material-tailwind/react";
-import { useNavigate } from "react-router-dom";
 import { useMediaquery } from "../../../hooks/usemediaQuery";
 import NavList from "./menu/NavList";
 import UserProfile from "../userprofile/UserProfile";
@@ -8,13 +7,11 @@ import { Menuopen } from "../../../constants";
 import { useDispatch } from "react-redux";
 import { OpenMobileviewNavbar } from "../../../redux/states/communitySlice";
 import MobileviewNavbar from "../mobileviewnavbar/MobileviewNavbar";
-import Cookies from "js-cookie";
 
 function Navigationbar() {
   const [openNav, setOpenNav] = useState(false);
   const isMobileView = useMediaquery(900);
   const dispatch = useDispatch();
-  const accessToken = Cookies.get("accessToken");
 
   const handleMobileViewNavBar = useCallback(() => {
     dispatch(OpenMobileviewNavbar());
@@ -30,20 +27,16 @@ function Navigationbar() {
       className="sticky shadow-none border-none bg-custom-greentransparent-gradient-t-b top-0 w-full"
     >
       <div className="flex items-center justify-between text-blue-gray-900 w-full">
-        <div className="flex gap-2">
-          <Typography className="text-lg md:text-2xl font-semibold">
-            <span className="text-4xl text-pastelGreen-600">THE </span>{" "}
-            <span className="text-3xl text-portgore-600">K</span>ISSAN - MITRA
-          </Typography>
-        </div>
-        <div className="flex items-center gap-4">
-          {accessToken ? (
-            <UserProfile />
-          ) : (
-            <div className="hidden gap-5 lg:flex">
-              <NavList />
-            </div>
-          )}
+        <Typography className="text-lg md:text-2xl font-semibold">
+          <span className="text-4xl text-pastelGreen-600">THE </span>{" "}
+          <span className="text-4xl text-portgore-600">K</span>ISSAN -{" "}
+          <span className="text-4xl text-portgore-600">M</span>ITRA
+        </Typography>
+        <div className="hidden gap-5 lg:flex">
+          <div className="hidden px-5 lg:block">
+            <NavList />
+          </div>
+          <UserProfile />
         </div>
         <IconButton
           variant="text"
